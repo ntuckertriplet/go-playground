750b351ab23a4c51dbac30380b692809a4471b3a
Nate <ntuckertriplet@gmail.com>
2019-05-27 11:36:54 +0000

    vars

--- go-crash-course/02_vars/main.go
+++ go-crash-course/02_vars/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Second go program")
+	//using vars
+	//type is inferred
+	var name = "nate"
+	fmt.Println(name)
+
+	//again, type is inferred
+	var age = 18
+	fmt.Println(name, age)
+
+	//to find the type, use a formatter
+	fmt.Printf("%T\n", name)
+
 }
