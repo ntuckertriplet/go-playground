367f5e499278ad8a2e809a3944d0643687479311
Nate <ntuckertriplet@gmail.com>
2019-05-28 22:26:45 +0000

    vars and notation

--- go-crash-course/02_vars/main.go
+++ go-crash-course/02_vars/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//can declare global
+var name = "Nate"
+
 func main() {
 	//using vars
 	//type is inferred
@@ -12,7 +15,19 @@ func main() {
 	var age = 18
 	fmt.Println(name, age)
 
-	//to find the type, use a formatter
-	fmt.Printf("%T\n", name)
+	//notation can only be used within function body
+	//Notice, can only declare initially, not redeclare
+	//Needs to be a new variable
+	newName := "Nathan"
+
+	//to find the type, use Printf and a formatter
+	fmt.Printf("%T\n%T\n", newName, age)
+
+	size := 3.87
+	fmt.Printf("%T\n", size)
+
+	//can declare multiple variables on same line
+	customerName, email := "nathan", "nate@email.com"
+	fmt.Println(customerName, email)
 
 }
