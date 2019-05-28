895d95fd1a56076694fb1cf298f2d047e9bcf849
Nate <ntuckertriplet@gmail.com>
2019-05-28 22:48:32 +0000

    learning functions

---
+++ go-crash-course/03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/ntuckertriplet/go-crash-course/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.7))
+	fmt.Println(strutil.Reverse("nate"))
+}
